ecbf0cfda9cda9b3eee74f16e185d7eca2c86d96
tonnyrogers <antoniel15975@gmail.com>
2020-04-24 21:24:47 -0300

    Reinport files

--- modulo03/gym/instructos.js
+++ modulo03/gym/instructos.js
@@ -5,20 +5,32 @@ exports.post = function(req,res){
     const keys = Object.keys(req.body)
 
     for( key of keys){
-        if( req.body[key] == "" ) 
-            return res.send(`Please fill all fields`)
+        if( req.body[key] == "" ){
+            return res.send("Please fill all fields")
+        }
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let { avatar_url, name, services, gender, birth } = req.body 
 
-    data.instructors.push(req.body)
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        birth,
+        name,
+        services,
+        gender,
+        created_at
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
         if(err) return res.send("Write file error")
 
-        return res.redirect("/")
+        return res.redirect('/instructors')
     })
 
-    return res.redirect("/instructors")
+    return res.redirect('/instructors')
 }
