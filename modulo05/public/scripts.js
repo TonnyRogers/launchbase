f8c6d78c41afba34fd32b80fb95f5d75792a6391
tonnyrogers <antoniel15975@gmail.com>
2020-05-03 22:47:32 -0300

    Image exibition

--- modulo05/public/scripts.js
+++ modulo05/public/scripts.js
@@ -46,10 +46,25 @@ const PhotosUpload = {
 
     },
     hasLimit(event){
-        const { uploadLimit, input: fileList } = PhotosUpload
+        const { uploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
 
         if(fileList.length > uploadLimit){
             alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return false
+        }
+
+        const photosDiv = []
+        preview.childNodes.forEach( item => {
+            if(item.classList && item.classList.value == "photo"){
+                photosDiv.push(item)
+            }
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+        if(totalPhotos > uploadLimit){
+            alert('Você atingiu o limite máximo de fotos')
             event.preventDefault()
             return false
         }
