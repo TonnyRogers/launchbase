523a2aeb7dd3dfc44112c4d06f793da3b727e09a
tonnyrogers <antoniel15975@gmail.com>
2020-05-10 15:51:08 -0300

    Product Search and List

--- modulo05/src/app/models/Product.js
+++ modulo05/src/app/models/Product.js
@@ -1,6 +1,12 @@
 const db = require('../../config/db')
 
 module.exports = {
+    all(){
+        return db.query(`
+            SELECT * FROM products
+            ORDER BY updated_at DESC
+        `)
+    },
     create(data){
         const query = `
             INSERT INTO products (
@@ -69,5 +75,33 @@ module.exports = {
     },
     files(id) {
         return db.query(`SELECT * FROM files WHERE product_id = $1`, [id])
+    },
+    search(params){
+        const { filter, category } = params
+
+        let query = '',
+            filterQuery = `WHERE`
+
+        filterQuery = `
+            ${filterQuery} ( products.name ILIKE '%${filter}%'
+            OR products.description ILIKE '%${filter}%' )
+        `
+
+        if(category){
+            filterQuery += `
+            AND products.category_id = ${category}
+            `
+        }
+
+        query = `
+            SELECT 
+            products.*,
+            categories.name AS category_name
+            FROM products
+            LEFT JOIN categories ON (categories.id = products.category_id)
+            ${filterQuery}
+        `
+
+        return db.query(query)
     }
 }
