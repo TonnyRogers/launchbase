666532a8a9fda5bfe5facabe63ff6ef81adac357
tonnyrogers <antoniel15975@gmail.com>
2020-05-27 23:48:40 -0300

    Cart Visual and Logical Handle Completed

--- modulo07/src/lib/cart.js
+++ modulo07/src/lib/cart.js
@@ -16,9 +16,77 @@ const Cart = {
 
         return this
     },
-    addOne(product){},
-    removeOnde(productId){},
-    delete(productId){}
+    addOne(product){
+        let inCart = this.getCartItem(product.id)
+        
+        if(!inCart){
+            inCart = {
+                product:{
+                    ...product,
+                    formattedPrice: formatPrice(product.price)
+                },
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0)            
+            }
+
+            this.items.push(inCart)
+        }
+
+        if(inCart.quantity >= product.quantity) return this
+
+        inCart.quantity++
+        inCart.price = inCart.product.price * inCart.quantity
+        inCart.formattedPrice = formatPrice(inCart.price)
+
+
+        this.total.quantity++
+        this.total.price += inCart.product.price
+        this.total.formattedPrice = formatPrice(this.total.price)
+
+        return this
+    },
+    removeOne(productId){
+        let inCart = this.getCartItem(productId)
+
+        if(! inCart) return this
+
+        inCart.quantity--
+        inCart.price = inCart.product.price * inCart.quantity
+        inCart.formattedPrice = formatPrice(inCart.price)
+
+        this.total.quantity--
+        this.total.price -= inCart.product.price
+        this.total.formattedPrice = formatPrice(this.total.price)
+
+        if(inCart.quantity < 1){
+            const itemIdex = this.items.indexOf(inCart)
+            this.items.splice(itemIdex,1)
+            return this
+        }
+
+        return this
+
+    },
+    delete(productId){
+
+        const inCart = this.getCartItem(productId)
+
+        if(!inCart) return this
+
+        if(this.items.length > 0){
+            this.total.quantity -= inCart.quantity
+            this.total.price -= (inCart.product.price * inCart.quantity)
+            this.total.formattedPrice = formatPrice(this.total.price)
+        }
+
+        this.items = this.items.filter( item => item.product.id != inCart.product.id)
+        return this
+
+    },
+    getCartItem(productId){
+        return this.items.find( item => item.product.id == productId)
+    }
 }
 
 module.exports = Cart
--- modulo07/src/routes/index.js
+++ modulo07/src/routes/index.js
@@ -5,11 +5,13 @@ const HomeController = require('../app/controllers/HomeController')
 
 const users = require('./users')
 const products = require('./products')
+const cart = require('./cart')
 
 routes.get('/', HomeController.index)
 
 routes.use('/users', users)
 routes.use('/products', products)
+routes.use('/cart', cart)
 
 routes.get('/ads/create', function(req,res){
     return res.redirect('/products/create');
